05b92b634e01fc2cd9515f867b1e93e3e42b780f
GeniusMind9999 <thenoobcoder999@gmail.com>
2024-10-06 15:23:36 +0530

    integrated the 3JS solar system model in the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import Orrery from './Orrery';
 
 function App() {
   return (
-    <div>
-      <div className='hero'>
-        <video autoPlay loop muted playsInline>
-          <source src="/space1.mp4" type="video/mp4" />
-        </video>
-        <div className='content'>
-          <h1 className='title'>Orrery Wep App</h1>
+    <>
+      <div>
+        <div className='hero'>
+          <video autoPlay loop muted playsInline className='video'>
+            <source src="/space1.mp4" type="video/mp4" />
+          </video>
+          <div className='content'>
+            <h1 className='title'>Orrery Wep App</h1>
+          </div>
         </div>
       </div>
-    </div>
+      <Orrery />
+    </>
   )
 }
 
-export default App
+export default App;
---
+++ src/Orrery.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+import "./Orrery.css";
+import { handleSpeedChange, handlePlanetsCheckbox, handleAsteroidsCheckbox, loadOrrery } from "./script.js";
+
+export default function Orrery() {
+    const [speed, setSpeed] = useState(1);
+    const [checkboxesChecked, setCheckboxesChecked] = useState([true, true]);
+    const speedLabelRef = useRef();
+    const planetsCheckboxRef = useRef();
+    const asteroidsCheckboxRef = useRef();
+
+    useEffect(loadOrrery, []);
+
+    function handleSliderChange(e) {
+        setSpeed(e.target.value);
+        handleSpeedChange(e, speedLabelRef.current);
+    }
+
+
+    return (
+        <div className="orrery">
+            <div className="speed-controller">
+                <label htmlFor="speedRange">Speed: </label>
+                <input
+                    type="range"
+                    id="speedRange"
+                    min="-10"
+                    max="10"
+                    step="1"
+                    value={speed}
+                    onInput={handleSliderChange}
+                />
+                <span
+                    className="speedValue"
+                    ref={speedLabelRef}
+                >
+                    1
+                </span>
+            </div>
+            <div className="dropdown-wrapper">
+                <div className="dropdown">
+                    <button className="dropbtn">Select Options</button>
+                    <div className="dropdown-content">
+                        <label><input
+                            className="planet-orbits"
+                            type="checkbox"
+                            value="Option 1"
+                            defaultChecked
+                            onChange={() => {
+                                const newCheckBoxesChecked = checkboxesChecked;
+                                newCheckBoxesChecked[0] = !checkboxesChecked[0];
+                                setCheckboxesChecked(newCheckBoxesChecked);
+                                handlePlanetsCheckbox(planetsCheckboxRef.current)
+                            }}
+                            ref={planetsCheckboxRef}
+                        />
+                            Planet Orbits
+                        </label>
+                        <label><input
+                            className="asteroid-orbits"
+                            type="checkbox"
+                            value="Option 2"
+                            defaultChecked
+                            onChange={() => {
+                                const newCheckBoxesChecked = checkboxesChecked;
+                                newCheckBoxesChecked[1] = !checkboxesChecked[1];
+                                setCheckboxesChecked(newCheckBoxesChecked);
+                                handleAsteroidsCheckbox(asteroidsCheckboxRef.current);
+                            }}
+                            ref={asteroidsCheckboxRef}
+                        />
+                            Asteroid Orbits
+                        </label>
+                        <label><input
+                            className="ph-asteroid-orbits"
+                            type="checkbox"
+                            value="Option 3"
+                        />
+                            Potentially Hazardous Asteroid Orbits
+                        </label>
+                        <label><input type="checkbox" value="Option 4" /> Option 4</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
